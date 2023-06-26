5570c3227e948fe1df764265fcfb53106b2e775d
JuliaRoughley <roughleyjulia@gmail.com>
2023-06-26 11:58:24 +0100

    further bug updates

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -12,6 +12,10 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    """Get all posts or create a new post. If the request method is GET: Return all posts in their original
+     order or sorted based on provided parameters.
+     If the request method is POST: Create a new post with the provided JSON data.
+     Returns JSON response containing the list of posts or the newly created post."""
     if request.method == 'POST':
         new_post = request.get_json()
         if not validate_post_data(new_post):
@@ -22,7 +26,7 @@ def get_posts():
             return jsonify({"error": "Post already exists"}), 409
 
         new_id = max(post['post_id'] for post in POSTS) + 1
-        new_post['id'] = new_id
+        new_post['post_id'] = new_id
 
         POSTS.append(new_post)
         return jsonify(new_post), 201
@@ -45,6 +49,8 @@ def get_posts():
 
 @app.route('/api/posts/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
+    """Post_id to be deleted is the parameter, and the post is deleted. If no post is found an error message is returned
+    """
     post = find_post_by_id(post_id)
     if post is None:
         return jsonify({"error": "Post not found"}), 404
@@ -54,6 +60,8 @@ def delete_post(post_id):
 
 @app.route('/api/posts/<int:post_id>', methods=['PUT'])
 def update_post(post_id):
+    """Update a post with the given post_id.The post_id of the post to be updated is the parameter.
+    Returns: JSON response saying post updated, and error 404 message post not found otherwise"""
     post = find_post_by_id(post_id)
     if post is None:
         return jsonify({"error": "Post not found"}), 404
@@ -69,7 +77,9 @@ def update_post(post_id):
 
 @app.route('/api/posts/search', methods=['GET'])
 def post_search():
-    post_id = request.args.get('id')
+    """Search for posts based on provided search terms. Returns JSON response containing the filtered
+     list of posts based on the provided search terms."""
+    post_id = request.args.get('post_id')
     content = request.args.get('content')
     title = request.args.get('title')
     filtered_posts = []
@@ -88,13 +98,15 @@ def post_search():
 
 
 def validate_post_data(data):
+    """ Validate the required fields of a new post. Takes the JSON data of the new post as parameter.
+    Returns True if the required fields are present, False otherwise."""
     if "title" not in data or "content" not in data:
         return False
     return True
 
 
 def find_existing_post(post):
-    """ Find if a post with the same title and content already exists. """
+    """ Find if a post with the same title and content already exists and returns it. None otherwise """
     for existing_post in POSTS:
         if existing_post['title'] == post['title'] and existing_post['content'] == post['content']:
             return existing_post
@@ -102,7 +114,7 @@ def find_existing_post(post):
 
 
 def find_post_by_id(post_id):
-    """Find the post with the id `post_id`.
+    """Finds and returns the post with the id `post_id`.
     If there is no post with this id, return None."""
     for post in POSTS:
         try:
@@ -114,22 +126,25 @@ def find_post_by_id(post_id):
 
 
 def is_valid_sort_field(field):
+    """ Checks if the provided sort field is valid. Returns True if the sort field is valid, False otherwise."""
     valid_sort_fields = ['title', 'content']
     return field in valid_sort_fields
 
 
 def is_valid_sort_direction(direction):
+    """ Check if the provided sort direction is valid. Returns True if the sort direction is valid, False otherwise."""
     valid_sort_directions = ['asc', 'desc']
     return direction in valid_sort_directions
 
 
 def sort_posts(field, direction):
+    """Sort the posts based on the provided field (title or content) and direction (ascending or descending).
+    Returns: The sorted list of posts."""
     if field == 'title':
         sorted_posts = sorted(POSTS, key=lambda post: post['title'])
     elif field == 'content':
         sorted_posts = sorted(POSTS, key=lambda post: post['content'])
     else:
-        # No sorting field provided, return original order
         return POSTS
 
     if direction == 'desc':
